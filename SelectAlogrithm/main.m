f65d17026b22fe3bd822cabe771d81f859ec35e0
Marvnc93 <42253000+Marvnc93@users.noreply.github.com>
2018-09-05 01:16:35 +0200

    First try with the select Algorithm

--- SelectAlogrithm/main.m
+++ SelectAlogrithm/main.m
@@ -1,16 +1,15 @@
 %programmDir = 'D:\Studienarbeit\ProgrammFolder\';
-loadDir = strcat('apptest_ROIX.mat');
-j=1;
 if exist('app') ==0
-app = load(loadDir);
+app = loadDir('apptest_ROIX.mat');
 app =app.app;
 end
+
 %-------------
 fig=figure('visible','on');
 button = uicontrol('style','pushbutton');
 set(button,'position',[1 1,120,20])
 set(button,'string','Next Picture')
-set(button,'callback',{@nextPicture,app,j})
+set(button,'callback',{@nextPicture,app})
 %app.ImageSelection.Slices_5.X_ROI{1,1}
 %Choose Number of Images per type
 %Types are XCathode XAnode YCathode YAnode
@@ -18,14 +17,12 @@ set(button,'callback',{@nextPicture,app,j})
 %1 mat for each picture with the image and the points
 % -> easy to save and implement
 % -> needs identifier for Type
-[x,y] = getpts;
 
-function nextPicture(object,~,app,j)
-test();
-j=j+1;
-end
-function test()
-i = randi([1,200],1,1)
+result=[nextPicture(button)];
+function result = nextPicture(object,~,app)
+i = randi([1,200],1,1);
 img= app.ImageSelection.Slices_5.X_ROI.narrowCathodeROI{i,1};
 imshow(img);
+[x,y] = getpts;
+result = [x,y];
 end
