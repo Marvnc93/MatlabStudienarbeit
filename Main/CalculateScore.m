0f4f6b56e594b3241df4e7795eddb23280d42c64
Marvnc93 <42253000+Marvnc93@users.noreply.github.com>
2018-09-12 00:24:50 +0200

    Created to create Score for IAandCD

---
+++ Main/CalculateScore.m
@@ -0,0 +1,29 @@
+function score = CalculateScore(pointsFound,userPoints,pointRange)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+%Sort Array Rows by second column
+pointsFound=sortrows(pointsFound,2);
+userPoints = sortrows(userPoints,2);
+
+Result = [];
+for i=1:size(userPoints,1)
+    for j=1:size(pointsFound,1)
+        Result(i,j)=calculateDistance(pointsFound(j,:),userPoints(i,:));
+    end
+    Result(i,j+1)=min(Result(i,1:j));
+end
+
+
+
+score = sum(Result(:,j+1) <=pointRange);
+
+
+    function distance = calculateDistance(xy1,xy2)
+        %Based on Pythagorean theorem
+        distance = sqrt((xy2(1)-xy1(1))^2+(xy2(2)-xy1(2))^2);
+    end
+
+
+end
+
