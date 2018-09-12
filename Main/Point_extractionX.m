754ba834248d61be4572fdf52bd88b02228e3795
Marvnc93 <42253000+Marvnc93@users.noreply.github.com>
2018-09-12 22:57:15 +0200

    Created

--- Main/Point_extractionX.m
+++ Main/Point_extractionX.m
@@ -2,104 +2,129 @@
 function Point_extractionX()
 %POINT_EXTRACTIONX Summary of this function goes here
 %   Detailed explanation goes here
-app = load('apptest.mat');
-app =app.app;
 
-for o=1:size(algorithmCell,1)
-    algorithm = algorithms{o};
-    for i=1:length(rOIandUserPoints)
-        Img = rOIandUserPoints{i,2};
-        greyValue =mean(Img,2);
-        numPoints = rOIandUserPoints{i,5};
+if exist('app','var') ==0
+    app = load('D:\Studienarbeit\ProgrammFolder\apptest_ROIX.mat');
+    app =app.app;
+end
+settings=load('D:\Studienarbeit\ProgrammFolder\settings.mat');
+settings=settings.settings;
+
+selectedValues=app.FolderSelection.InputFolders.Selected_Values;
+stepsForFigures =10;
+drawFigures = true;
+  %!!!!!!!!!!!!! A
+for i=1:length(selectedValues)
+    for j=1:size((app.ImageSelection.(selectedValues{i}).X_ROI.narrowCathodeROI),1)
+        ImgC=app.ImageSelection.(selectedValues{i}).X_ROI.narrowCathodeROI{j,1};
+        ImgA=app.ImageSelection.(selectedValues{i}).X_ROI.narrowAnodeROI{j,1};
+
+        pointsFoundC = GetPoints(ImgC,settings,14);
+        pointsFoundA = GetPoints(ImgA,settings,15); 
+        %% Loop for Images
+        if drawFigures==true && mod(j,stepsForFigures)==0
+            fig = figure('visible','off',...
+                'Position',[500 300 500 700]);
+            subplot(2,2,1)
+            imshow(ImgC);
+            hold on;
+            plot(pointsFoundC.Location(:,1),pointsFoundC.Location(:,2),'gx');
+            title('Cathode');
+            subplot(2,2,2)
+            imshow(ImgA);
+            hold on;
+            plot(pointsFoundA.Location(:,1),pointsFoundA.Location(:,2),'gx');
+            title('Anode');
+            subplot(2,2,[3 4]);
+            A =imread(app.ImageSelection.(selectedValues{i}).X_ROI.narrowCathodeROI{j,4});
+            imshow(A);
+            hold on;
+            plot(pointsFoundC.Location(:,1)+single(app.ImageSelection.(selectedValues{i}).X_ROI.narrowCathodeROI{j,2}-99),pointsFoundC.Location(:,2),'gx');
+            hold on;
+            plot(pointsFoundA.Location(:,1)+single(app.ImageSelection.(selectedValues{i}).X_ROI.narrowAnodeROI{j,2}-99),pointsFoundA.Location(:,2),'gx');
+            title('Full Picture');
+            saveas(fig,strcat('D:\Studienarbeit\ProgrammFolder\PointsX\Narrow\',int2str(i),filesep,int2str(j),'.png'));
+        end
+    end
+end
+for i=1:length(selectedValues)
+    for j=1:size((app.ImageSelection.(selectedValues{i}).X_ROI.broadCathodeROI),1)
+        ImgC=app.ImageSelection.(selectedValues{i}).X_ROI.broadCathodeROI{j,1};
+        ImgA=app.ImageSelection.(selectedValues{i}).X_ROI.broadAnodeROI{j,1};
+
+        pointsFoundC = GetPoints(ImgC,settings,14);
+        pointsFoundA = GetPoints(ImgA,settings,15); 
+        %% Loop for Images
+        if drawFigures==true && mod(j,stepsForFigures)==0
+            fig = figure('visible','off',...
+                'Position',[500 300 500 700]);
+            subplot(2,2,1)
+            imshow(ImgC);
+            hold on;
+            plot(pointsFoundC.Location(:,1),pointsFoundC.Location(:,2),'gx');
+            title('Cathode');
+            subplot(2,2,2)
+            imshow(ImgA);
+            hold on;
+            plot(pointsFoundA.Location(:,1),pointsFoundA.Location(:,2),'gx');
+            title('Anode');
+            subplot(2,2,[3 4]);
+            A =imread(app.ImageSelection.(selectedValues{i}).X_ROI.broadCathodeROI{j,4});
+            imshow(A);
+            hold on;
+            plot(pointsFoundC.Location(:,1)+single(app.ImageSelection.(selectedValues{i}).X_ROI.broadCathodeROI{j,2}-99),pointsFoundC.Location(:,2),'gx');
+            hold on;
+            plot(pointsFoundA.Location(:,1)+single(app.ImageSelection.(selectedValues{i}).X_ROI.broadAnodeROI{j,2}-99),pointsFoundA.Location(:,2),'gx');
+            title('Full Picture');
+            saveas(fig,strcat('D:\Studienarbeit\ProgrammFolder\PointsX\Broad\',int2str(i),filesep,int2str(j),'.png'));
+        end
+    end
+end
+    function pointsFound = GetPoints(Img,settings,numPoints)
+        greyCutoff = settings{1};
+        structureElement = settings{2};
+        structureElementShape=settings{3};
+        sharpen = settings{4};
+        algorithm = settings{5};
+        
         %% Grey Cutoff
-        if greyCutoff{o}=="on"
-            greyCutOffValues = mean(greyValue);
-            ImgAdj = imadjust(Img,[greyCutOffValues/255,1],[0 1]);
-        else
-            ImgAdj=Img;
+        if greyCutoff=="on"
+            greyCutOffValues = mean(mean(Img));
+            Img = imadjust(Img,[greyCutOffValues/255,1],[0 1]);;
         end
         %% Morphologie closing
-        if structureElement{o}=="on"
-            ses = structureElementShape{o};
+        if structureElement=="on"
+            ses = structureElementShape;
             shape = ses{1,1};
-            size =ses{1,2};
-            SE = strel(shape,size);
-            ImgAdj = imclose(ImgAdj,SE);
+            len =ses{1,2};
+            SE = strel(shape,len);
+            Img = imclose(Img,SE);
         end
         %% ImgSharpen
-        if sharpen{o}=="on"
-            ImgAdj = imsharpen(ImgAdj);
+        if sharpen=="on"
+            Img = imsharpen(Img);
         end
-        greyValueAdj = mean(ImgAdj,2);
         %% Find Harrison Features
         if algorithm =="Harrison"
             pointsFound = detectHarrisFeatures(Img,'Filtersize',7);
             pointsFound = pointsFound.selectStrongest(numPoints);
-            pointsFoundAdj = detectHarrisFeatures(ImgAdj,'Filtersize',7);
-            pointsFoundAdj = pointsFoundAdj.selectStrongest(numPoints);
+            
         end
         %% Find ShiTomasi Features
         if algorithm =="ShiTomasi"
             pointsFound = detectHarrisFeatures(Img,'Filtersize',7);
             pointsFound = pointsFound.selectStrongest(numPoints);
-            pointsFoundAdj = detectHarrisFeatures(ImgAdj,'Filtersize',7);
-            pointsFoundAdj = pointsFoundAdj.selectStrongest(numPoints);
         end
         %% Find SURF Features
         if algorithm =="SURF"
             pointsFound = detectSURFFeatures(Img);
             pointsFound = pointsFound.selectStrongest(numPoints);
-            pointsFoundAdj = detectSURFFeatures(ImgAdj);
-            pointsFoundAdj = pointsFoundAdj.selectStrongest(numPoints);
         end
         %% Find BRISK Features
         if algorithm == "BRISK"
             pointsFound = detectBRISKFeatures(Img);
             pointsFound = pointsFound.selectStrongest(numPoints);
-            pointsFoundAdj = detectBRISKFeatures(ImgAdj);
-            pointsFoundAdj = pointsFoundAdj.selectStrongest(numPoints);
-        end
-        %% Get Score
-        %Points found without image augmentation
-%        rOIandUserPoints{i,6} = CalculateScore(pointsFound.Location,rOIandUserPoints{i,1},pointRange);
-        %Points found with image augmentation
-        rOIandUserPoints{i,6} = CalculateScore(pointsFoundAdj.Location,rOIandUserPoints{i,1},pointRange);
-        
-        %% Loop for Images
-        if drawFigures==true
-            fig = figure('visible','off',...
-                'Position',[500 300 500 700]);
-            
-            subplot(2,2,1)
-            imshow(Img);
-            hold on;
-            plot(pointsFound.Location(:,1),pointsFound.Location(:,2),'rx');
-            hold on;
-            plot(rOIandUserPoints{i,1}(:,1),rOIandUserPoints{i,1}(:,2),'g+');
-            title('Image without Adjustment');
-            subplot(2,2,2)
-            
-            imshow(ImgAdj);
-            hold on;
-            plot(pointsFoundAdj.Location(:,1),pointsFoundAdj.Location(:,2),'rx');
-            hold on;
-            plot(rOIandUserPoints{i,1}(:,1),rOIandUserPoints{i,1}(:,2),'g+');
-            
-            title('Image after Adjustment');
-            subplot(2,2,[3 4]);
-            plot(greyValue);
-            hold on;
-            title(rOIandUserPoints{i,4});
-            plot(greyValueAdj,'g');
-            
-            
-            saveas(fig,strcat(path,filesep,'PointsX\',int2str(i),rOIandUserPoints{i,3},rOIandUserPoints{i,4},'.png'));
         end
     end
-    %algorithmCell{o,7} = CalculateScore(pointsFoundAdj.Location,rOIandUserPoints{i,1},pointRange);
-end
-
-
 
 end
-
