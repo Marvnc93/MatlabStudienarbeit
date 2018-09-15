b4a9f1f050e205f5b77340bb0bef65dd2b391285
Marvnc93 <42253000+Marvnc93@users.noreply.github.com>
2018-09-15 19:01:59 +0200

    Implement usage of angle to calculate distances

--- Main/CalcDistance.m
+++ Main/CalcDistance.m
@@ -1,4 +1,4 @@
-function CalcDistance(app,selectedValues,j,i,type)
+function CalcDistance(app,selectedValues,angle,j,i,type)
 %CALCDISTANCE Summary of this function goes here
 %   Detailed explanation goes here
 if type == "Narrow"
@@ -6,12 +6,14 @@ if type == "Narrow"
     pointsC = app.Distances.(selectedValues{i}).PointsX{j+1,1}(:,1);
     pointsA = app.Distances.(selectedValues{i}).PointsX{j+1,2}(:,1);
     app.Distances.(selectedValues{i}).DistancesX{j+1,1} = GetDistance(pointsC,pointsA);
+    app.Distances.(selectedValues{i}).DistancesX{j+1,2} = app.Distances.(selectedValues{i}).DistancesX{j+1,1}*sin((90+angle)/180*pi);
 end
 if type == "Broad"
     %Load the values according to PointsX - only x values relevant
     pointsC = app.Distances.(selectedValues{i}).PointsX{j+1,3}(:,1);
     pointsA = app.Distances.(selectedValues{i}).PointsX{j+1,4}(:,1);
     app.Distances.(selectedValues{i}).DistancesX{j+1,3} = GetDistance(pointsC,pointsA);
+    app.Distances.(selectedValues{i}).DistancesX{j+1,4} = app.Distances.(selectedValues{i}).DistancesX{j+1,3}*cos((90+angle)/180*pi);
 end
     function distances = GetDistance(pointsCathode,pointsAnode)
         cc =1;
--- Main/getXPoints.m
+++ Main/getXPoints.m
@@ -10,18 +10,26 @@ drawFigures = false;
 pointsFound = struct;
 settings=load(strcat(app.FolderSelection.Programm_Path,'\settings.mat'));
 settings=settings.settings;
+
+% angle = extractAngle(app.ImageSelection.Slices_5.Z)
+% actual_distance_narrow_Y = distance_narrow_Y*cos((90+image_acquisition_angle)/180*pi);
+% actual_distance_broad_Y = distance_broad_Y*sin((90+image_acquisition_angle)/180*pi);
+% actual_distance_narrow_X = distance_narrow_X * sin((90+image_acquisition_angle)/180*pi);
+% actual_distance_broad_X = distance_broad_X*cos((90+image_acquisition_angle)/180*pi);
+
 for i=1:length(selectedValues)
     app.Distances.(selectedValues{i}).PointsX = {"Narrow Cathode","Narrow Anode","Broad Cathode","Broad Anode"};
     app.Distances.(selectedValues{i}).DistancesX = {"Narrow raw","Narrow real", "Broad raw","Broad real"};
+    angle = extractAngle(app.ImageSelection.(selectedValues{i}).Z);
     for j=1:size((app.ImageSelection.(selectedValues{i}).X_ROI.narrowCathodeROI),1)
         Point_extractionX(app,settings,selectedValues,drawFigures,stepsForFigures,j,i,"Narrow")
         InterpolatePointsX(app,selectedValues,drawFigures,stepsForFigures,j,i,"Narrow")
-        CalcDistance(app,selectedValues,j,i,"Narrow")
+        CalcDistance(app,selectedValues,angle,j,i,"Narrow")
     end
         for j=1:size((app.ImageSelection.(selectedValues{i}).X_ROI.broadCathodeROI),1)
         Point_extractionX(app,settings,selectedValues,drawFigures,stepsForFigures,j,i,"Broad")
         InterpolatePointsX(app,selectedValues,drawFigures,stepsForFigures,j,i,"Broad")
-        CalcDistance(app,selectedValues,j,i,"Broad")
+        CalcDistance(app,selectedValues,angle,j,i,"Broad")
     end
 end
 
