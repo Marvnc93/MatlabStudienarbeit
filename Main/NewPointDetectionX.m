9bd4904064d9ac083211670bc553940bf7227059
Marvnc93 <42253000+Marvnc93@users.noreply.github.com>
2018-10-04 00:14:55 +0200

    Irrelevant -> GetFreqPoints

--- Main/NewPointDetectionX.m
+++ Main/NewPointDetectionX.m
@@ -12,19 +12,20 @@ stepsForFigures =10;
 drawFigures = true;
 % pointsFound = struct;
 
-for i=1:length(selectedValues)
+%for i=1:length(selectedValues)
+for i=1:1
     for j=1:size((app.ImageSelection.(selectedValues{i}).X_ROI.narrowCathodeROI),1)
         ImgC=app.ImageSelection.(selectedValues{i}).X_ROI.narrowCathodeROI{j,1};
 %         ImgA=app.ImageSelection.(selectedValues{i}).X_ROI.narrowAnodeROI{j,1};
         
         %% Cathode
-        meanGreyAdjC = mean(mean(ImgC,2))*2;
+        meanGreyAdjC = mean(mean(ImgC,2));
         ImgC = imadjust(ImgC,[meanGreyAdjC/255,1],[0 1]);
         meanGreyC=mean(ImgC,2);
         xC = transpose(1:length(meanGreyC));
-        yC = meanGreyC*1.5;
+        yC = meanGreyC*3;
         [pksC,locsC] = findpeaks(yC,'MinPeakDistance',10);
-        [MC,IC] = maxk(pksC,14);
+        [MC,IC] = maxk(pksC,15);
 %         %% Anode
 %         meanGreyAdjA = mean(mean(ImgA,2));
 %         ImgA = imadjust(ImgA,[meanGreyAdjA/255,1],[0 1]);
@@ -64,9 +65,9 @@ for i=1:length(selectedValues)
         ImgC = imadjust(ImgC,[meanGreyAdjC/255,1],[0 1]);
         meanGreyC=mean(ImgC,2);
         xC = transpose(1:length(meanGreyC));
-        yC = meanGreyC*1.5;
+        yC = meanGreyC*3;
         [pksC,locsC] = findpeaks(yC,'MinPeakDistance',10);
-        [MC,IC] = maxk(pksC,14);
+        [MC,IC] = maxk(pksC,15);
 %         %% Anode
 %         meanGreyAdjA = mean(mean(ImgA,2))*1.2;
 %         ImgA = imadjust(ImgA,[meanGreyAdjA/255,1],[0 1]);
@@ -98,7 +99,7 @@ for i=1:length(selectedValues)
 end
 %meanGrey = mean(Img((size(Img,2)/2-50):(size(Img,2)/2+50)));
 
-
+close all;
 
 
 
