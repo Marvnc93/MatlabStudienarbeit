91cff357bf527978c57f8d03409171e5e8595603
Marvnc93 <42253000+Marvnc93@users.noreply.github.com>
2018-10-04 00:12:32 +0200

    Just testing

--- For Paper/Morphologie.m
+++ For Paper/Morphologie.m
@@ -1,11 +1,16 @@
 Img = imread('corners.jpg');
-ImgB=imbinarize(Img);
+maxImg = max(max(Img));
+ImgB =imadjust(Img, [193*0.7/255 1],[0 1]);
+SE = strel('rectangle',[5,1]);
+ImgB=imbinarize(ImgB);
+ImgB =imerode(ImgB,SE);
 % se = strel('square',5);
 % %closeBW = imclose(ImgB,se);
 % closeBW = imerode(ImgB,se);
 %could be interesting for outlook
 closeBW = bwmorph(ImgB,'remove');
 subplot(1,2,1)
-imshow(ImgB);
+%imhist(Img);
+imshow(Img);
 subplot(1,2,2)
-imshow(closeBW);
+imshow(ImgB);
