7b32e53424522a06ce3be90badf6b83d6d1b1f1d
Marvnc93 <42253000+Marvnc93@users.noreply.github.com>
2018-10-07 00:38:49 +0200

    Is now a function

--- Main/getXPoints.m
+++ Main/getXPoints.m
@@ -1,13 +1,13 @@
-% this should call all function related to X
-%% Real Part
-% app = load('apptest.mat');
-% app =app.app;
-% ROI_extractionX(app)
-%% Debug
-if exist('app','var') ==0
-    app = load('D:\Studienarbeit\ProgrammFolder\apptest_ROIX.mat');
-    app =app.app;
-end
+function  getXPoints(app)
+% %% Real Part
+% % app = load('apptest.mat');
+% % app =app.app;
+% % ROI_extractionX(app)
+% %% Debug
+% if exist('app','var') ==0
+%     app = load('D:\Studienarbeit\ProgrammFolder\apptest_ROIX.mat');
+%     app =app.app;
+% end
 selectedValues=app.FolderSelection.InputFolders.Selected_Values;
 stepsForFigures =10;
 drawFigures = false;
@@ -23,23 +23,32 @@ Settings=Settings.Settings;
 
 %% real part
 for i=1:length(selectedValues)
-%        for i=3:3
+    %        for i=3:3
+    i
     app.Distances.(selectedValues{i}).PointsX = {"Narrow Cathode","Narrow Anode","Broad Cathode","Broad Anode"};
     app.Distances.(selectedValues{i}).DistancesX = {"Narrow raw per Image","Narrow real per Image","Distances raw per point", "Distances real per point",...
         "Broad raw per image","Broad real per image","Distances Raw per point", "Distances Real per point"};
-        %% angle needs to be applied in a different fashion for both sides!!!!!!!!
+    app.Distances.(selectedValues{i}).ResultsX = {"Narrow","Broad"};
+    %% angle needs to be applied in a different fashion for both sides!!!!!!!!
     angle = extractAngle(app.ImageSelection.(selectedValues{i}).Z);
-    for j=1:size((app.ImageSelection.(selectedValues{i}).X_ROI.narrowCathodeROI),1)
-        Point_extractionX(app,Settings,selectedValues,drawFigures,stepsForFigures,j,i,"Narrow")
-        CalcDistance(app,selectedValues,angle,j,i,"Narrow")
-    end
+        for j=1:size((app.ImageSelection.(selectedValues{i}).X_ROI.narrowCathodeROI),1)
+            if mod(j,10)==0
+                strcat("Narrow at: ",int2str(j),"/",int2str(size((app.ImageSelection.(selectedValues{i}).X_ROI.narrowCathodeROI),1)))
+            end
+            Point_extractionX(app,Settings,selectedValues,drawFigures,stepsForFigures,j,i,"Narrow")
+            CalcDistance(app,selectedValues,angle,j,i,"Narrow","X")
+        end
     for j=1:size((app.ImageSelection.(selectedValues{i}).X_ROI.broadCathodeROI),1)
+        if mod(j,10)==0
+            strcat("Broad at: ",int2str(j),"/",int2str(size((app.ImageSelection.(selectedValues{i}).X_ROI.broadCathodeROI),1)))
+        end
         Point_extractionX(app,Settings,selectedValues,drawFigures,stepsForFigures,j,i,"Broad")
-        CalcDistance(app,selectedValues,angle,j,i,"Broad")
+        CalcDistance(app,selectedValues,angle,j,i,"Broad","X")
     end
-    StatisticDistances(app,selectedValues,drawFigures,stepsForFigures,i,"Narrow");
-    StatisticDistances(app,selectedValues,drawFigures,stepsForFigures,i,"Broad");
+    StatisticDistancesX(app,selectedValues,drawFigures,stepsForFigures,i,"Narrow");
+    StatisticDistancesX(app,selectedValues,drawFigures,stepsForFigures,i,"Broad");
 end
+
 
 
 
